59c4fb5201f7408da08dbe563e6bb8b4ed1f55b2
Manish Kumar <manish.jangid08@gmail.com>
2021-09-19 14:08:28 -0700

    Adding new program

--- Sliding_Window/max_sum_contiguous_window.cpp
+++ Sliding_Window/max_sum_contiguous_window.cpp
@@ -1,1 +1,25 @@
+using namespace std;
 
+#include <iostream>
+#include <vector>
+
+class MaxSumSubArrayOfSizeK {
+ public:
+  static int findMaxSumSubArray(int k, const vector<int>& arr) {
+    int maxSum = 0;
+    int running_sum = 0;
+    // TODO: Write your code here
+    int windowStart = 0;
+    int windowEnd = 0;
+    for (windowEnd = 0; windowEnd < arr.size(); windowEnd++) {
+        running_sum += arr[windowEnd];
+
+        if (windowEnd >= (k - 1)) {
+            maxSum = max(maxSum, running_sum);
+            running_sum -= arr[windowStart];
+            windowStart++;
+        }
+    }
+    return maxSum;
+  }
+};
